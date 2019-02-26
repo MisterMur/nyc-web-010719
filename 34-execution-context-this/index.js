03cc753af50b454dc8b163bfb6952090f49a97b3
Wangtron <evansjwang@gmail.com>
2019-02-26 18:37:26 -0500

    badboys push to master

---
+++ 34-execution-context-this/index.js
@@ -0,0 +1,79 @@
+
+console.log(this)
+
+
+
+const dog = {
+  name: 'winfield',
+  favSnacks: ['cabbage', 'carrots', 'bones'],
+  arrowEatSnacks: function() {
+    this.favSnacks.forEach( snack => {
+      console.log(`${this.name} is eating ${snack}`)
+    })
+  },
+  regEatSnacks: function() {
+    this.favSnacks.forEach(function(snack) {
+      console.log(`${this.name} is eating ${snack}`)
+    }.bind(dog))
+  }
+}
+
+
+
+console.log(filtered)
+this.name = 'window object'
+
+// /************ Function Called with New Keyword ***********/
+// //inside a constructor fn, this will be the newly created object
+//
+// const personConstructor = function(name) {
+//   // `this` within a constructor will be the newly created object
+//   // { name: name }
+//   this.name = name
+// }
+//
+// const evans = new personConstructor('evans')
+//
+// function onTheGlobal() {
+//   console.log(this)
+// }
+//
+// const instructor = {
+//   name: "POWERFUL MC",
+//   lecture: function(){
+//     return `${this.name} gives POWERFUL lectures ;)`
+//   }
+// }
+//
+//
+// /********************************************************/
+//
+// /************ Bind Call Apply ****************************/
+//
+//
+//
+// function myForEach(arr, callback) {
+//   for (let i = 0; i < arr.length; i++) {
+//     callback(arr[i])
+//   }
+// }
+//
+//
+// // bind returns a COPY of our fn with `this` fixed to whatever obj was passed as an arg
+//
+// /********************************************************/
+//
+// /************ Function called on an Object ***************/
+//
+//
+// /********************************************************/
+//
+// /************ Simple Function Call ***********************/
+//
+//
+// /********************************************************/
+//
+// /************ Arrow Functions ****************************/
+//
+//
+// /********************************************************/
